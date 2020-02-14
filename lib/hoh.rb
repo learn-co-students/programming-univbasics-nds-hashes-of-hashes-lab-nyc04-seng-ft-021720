97ad7f0bc90214b3b73751ce319d23e35cc9d621
concurrent-server-0630 <andycruz008@gmail.com>
2020-02-14 02:31:33 +0000

    Done.

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -31,6 +31,40 @@
 # }
 
 def naming_system
+ life1 = {
+  label: "Kingdom",
+  sub_category: {
+  label: "Phylum",
+ sub_category: {
+  label: "Class",
+ sub_category: {
+   label: "Order",
+   sub_category: {
+  label: "Family",
+ sub_category: {
+  label: "Genus",
+ sub_category: {
+   label: "Species",
+   sub_category: nil
+    }
+  }
+ }
+ }
+ }}}
+ 
+    
+   
+   
+   
+   
+   
+   
+   
+
+  
+  
+  
+  
   # Remember:
   #  Kingdom
   #  Phylum
@@ -41,5 +75,6 @@ def naming_system
   #  Species
   # So, if we have the "Kingdom" node we should be able to "tunnel" into the
   # HoH all the way to Species!
+life1
 
 end
